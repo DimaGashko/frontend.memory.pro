51bc66c0f0d86eee4fc43e40c6fea4f0d9fb67fc
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-21 03:29:19 +0200

    set up nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,9 +33,9 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    '@nuxtjs/eslint-module'
     // Doc: https://github.com/nuxt-community/stylelint-module
-    '@nuxtjs/stylelint-module'
+    // '@nuxtjs/stylelint-module'
   ],
   /*
    ** Nuxt.js modules
@@ -60,4 +60,4 @@ export default {
      */
     extend(config, ctx) {}
   }
-}
+};
