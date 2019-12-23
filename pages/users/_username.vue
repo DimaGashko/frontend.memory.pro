457c2f7a1931fb8830494fe5f3ec5f736ab2c98a
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-23 13:19:41 +0200

    base user profile

--- pages/users/_username.vue
+++ pages/users/_username.vue
@@ -1,11 +1,41 @@
 <template>
   <div>
-    <h1>User ({{ $route.params.username }})</h1>
+    <h1 v-if="!ownProfile">Firbidden!</h1>
+    <div v-else>
+      <h1 v-if="ownProfile">
+        Hello, {{ $auth.user.first_name }} {{ $auth.user.last_name }}
+      </h1>
+
+      <ul class="data-list">
+        <li class="data-list__item">Username: {{ $auth.user.username }}</li>
+        <li class="data-list__item">Email: {{ $auth.user.email }}</li>
+        <li class="data-list__item">First name: {{ $auth.user.first_name }}</li>
+        <li class="data-list__item">Last name: {{ $auth.user.last_name }}</li>
+        <li class="data-list__item">Birth Date: {{ $auth.user.birth }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    ownProfile() {
+      return this.$route.params.username === this.$auth.user.username;
+    }
+  }
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.data-list {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin-top: 1em;
+}
+
+.data-list__item {
+  margin-bottom: 0.8em;
+}
+</style>
