edddd6d0854f81b64e6eeb48d2b3354f7af743a3
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-25 05:55:19 +0200

    controllers styles

--- components/training/RememberingControllers.vue
+++ components/training/RememberingControllers.vue
@@ -1,16 +1,16 @@
 <template>
   <ul class="list">
-    <li class="list__item">
-      <b-button>Prev</b-button>
+    <li>
+      <b-button block size="lg">Prev (Space)</b-button>
     </li>
-    <li class="list__item">
-      <b-button>Next</b-button>
+    <li>
+      <b-button block size="lg">Next (Enter)</b-button>
     </li>
-    <li class="list__item">
-      <b-button>First</b-button>
+    <li>
+      <b-button block size="lg">First (Alt + Space)</b-button>
     </li>
-    <li class="list__item">
-      <b-button>Finished</b-button>
+    <li>
+      <b-button block size="lg">Finished (Alt + Enter)</b-button>
     </li>
   </ul>
 </template>
@@ -20,8 +20,11 @@
   list-style: none;
   padding: 0;
   margin: 0;
-}
 
-.list__item {
+  display: grid;
+  grid-template: '1fr 1fr';
+  gap: 1em;
+
+  margin-top: 32px;
 }
 </style>
