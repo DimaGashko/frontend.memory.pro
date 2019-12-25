b233a66b338b7d9f23ca7f44e5151ba7a5db85da
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-25 05:22:12 +0200

    inited Controllers

--- components/training/Memorizing.vue
+++ components/training/Memorizing.vue
@@ -1,13 +1,34 @@
 <template>
   <div>
-    <WordsItem value="Greatness" even="true" />
+    <SimpleDataItem :value="word" :even="even" />
+    <RememberingControllers />
   </div>
 </template>
 
 <script>
-import WordsItem from '@/components/training/WordsItem';
+import SimpleDataItem from '@/components/training/SimpleDataItem';
+import RememberingControllers from '@/components/training/RememberingControllers';
 
 export default {
-  components: { WordsItem }
+  components: { SimpleDataItem, RememberingControllers },
+  data: () => ({
+    word: 'Greatness',
+    even: false
+  }),
+  created() {
+    const words = [
+      '43     444    23',
+      '11     444    44',
+      '33     333    33',
+      '23     775    77',
+      '33     222    666'
+    ];
+    let i = 0;
+    setInterval(() => {
+      this.word = words[i % words.length];
+      this.even = i % 2 === 0;
+      i++;
+    }, 500);
+  }
 };
 </script>
---
+++ components/training/RememberingControllers.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="list">
+    <li class="list__item">
+      <b-button>Prev</b-button>
+    </li>
+    <li class="list__item">
+      <b-button>Next</b-button>
+    </li>
+    <li class="list__item">
+      <b-button>First</b-button>
+    </li>
+    <li class="list__item">
+      <b-button>Finished</b-button>
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+.list {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.list__item {
+}
+</style>
