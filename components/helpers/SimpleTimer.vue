0fc627459544b81bcd54629533af85ef1b9cd622
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-25 17:27:59 +0200

    SimpleTimer

---
+++ components/helpers/SimpleTimer.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>{{ rest | time }}</div>
+</template>
+
+<script>
+export default {
+  props: ['time'],
+  data: () => ({
+    timer: 0,
+    rest: 0,
+    updateInterval: 250,
+    startAt: 0
+  }),
+  created() {
+    this.start();
+  },
+  methods: {
+    done() {
+      this.stop();
+      this.$emit('done');
+    },
+    start() {
+      this.stop();
+      this.rest = this.time;
+      this.startAt = Date.now();
+
+      this.timer = setInterval(() => {
+        this.tik();
+      }, this.updateInterval);
+    },
+    stop() {
+      clearInterval(this.timer);
+      this.timer = 0;
+    },
+    tik() {
+      this.rest = this.time - (Date.now() - this.startAt) / 1000;
+      if (this.rest < 0) this.rest = 0;
+
+      if (this.rest === 0) {
+        this.done();
+      }
+    }
+  }
+};
+</script>
