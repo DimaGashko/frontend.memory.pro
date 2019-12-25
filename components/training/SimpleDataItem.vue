fc39817df9f65d36d713fb18da14c9530f600f23
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-25 05:21:46 +0200

    SimpleDataItem

---
+++ components/training/SimpleDataItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <pre :class="{ 'word--even': even }" class="word">{{ value }}</pre>
+</template>
+
+<script>
+export default {
+  props: ['value', 'even']
+};
+</script>
+
+<style lang="scss" scoped>
+.word {
+  margin: 0;
+  padding: 1em 0;
+
+  font-size: 72px;
+  text-align: center;
+
+  &--even {
+    background-color: rgb(240, 240, 240);
+  }
+}
+</style>
