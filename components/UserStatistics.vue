481e2a39f571299f9b12321c771d2c88be09c773
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-26 20:47:29 +0200

    fields

--- components/UserStatistics.vue
+++ components/UserStatistics.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <h2>Words Results</h2>
-    <b-table :items="wordsResults" striped hover></b-table>
+    <b-table
+      :items="wordsResults"
+      :fields="wordsResultsFields"
+      striped
+      hover
+    ></b-table>
   </div>
 </template>
 
@@ -13,7 +18,15 @@ export default {
   data: () => ({
     wordsResults: [],
     numbersResults: [],
-    imagesResults: []
+    imagesResults: [],
+    wordsResultsFields: [
+      { key: 'id', sortable: true },
+      { key: 'start_at', sortable: true },
+      { key: 'preparation_time', sortable: true },
+      { key: 'recall_preparation_time', sortable: true },
+      { key: 'recall_time', sortable: true },
+      { key: 'template', sortable: true }
+    ]
   }),
   created() {
     this.fetchData();
