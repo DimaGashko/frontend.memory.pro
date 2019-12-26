4d0f31aa8f788b97a762d3b1efb8c6dbf6d79e16
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-26 05:49:44 +0200

    Recall

--- components/train/steps/Recall.vue
+++ components/train/steps/Recall.vue
@@ -1,23 +1,34 @@
 <template>
   <div>
     <div class="fields">
-      f1 f2 f3...
+      <RecallItem ref="items" v-for="n in len" :key="n" :index="n" />
     </div>
     <div class="controls mt-4">
-      <b-button size="lg">Continue</b-button>
+      <b-button @click="done" size="lg">Continue</b-button>
     </div>
   </div>
 </template>
 
 <script>
+import RecallItem from '@@/train/RecallItem';
+
 export default {
-  mounted() {
-    setTimeout(() => {
-      this.$emit('done');
-    }, 2000);
+  components: { RecallItem },
+  props: ['len'],
+  computed: {
+    answers() {
+      return this.$refs.items.map(item => item.value);
+    }
+  },
+  methods: {
+    done() {
+      this.$emit('done', this.answers);
+    }
   }
 };
 </script>
+
+<style lang="scss" scoped></style>
 
 <style lang="scss" scoped>
 .controls {
