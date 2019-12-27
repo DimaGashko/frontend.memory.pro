a907fce23991bee7b5cbbef1e2a64d9e7d4568bb
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-27 03:14:36 +0200

    save methods

--- store/results.js
+++ store/results.js
@@ -3,9 +3,25 @@ import { processRequestError } from '~/assets/scripts/utils.ts';
 export const state = () => ({});
 
 export const actions = {
+  async saveNumbersResult(_, data) {
+    try {
+      return await this.$axios.$put(`/results/numbers`, data);
+    } catch (e) {
+      processRequestError(e);
+    }
+  },
+
   async saveWordsResult(_, data) {
     try {
       return await this.$axios.$put(`/results/words`, data);
+    } catch (e) {
+      processRequestError(e);
+    }
+  },
+
+  async saveImagesResult(_, data) {
+    try {
+      return await this.$axios.$put(`/results/images`, data);
     } catch (e) {
       processRequestError(e);
     }
