0e197fc27219bcc2249bde5eac16d945686d4e22
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-27 06:46:12 +0200

    Minimal results pages

---
+++ pages/results/numbers/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <pre>{{ data }}</pre>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  data: () => ({
+    data: null
+  }),
+  async created() {
+    this.data = await this.fetch(this.$route.params.id);
+  },
+  methods: {
+    ...mapActions({
+      fetch: 'results/getNumbersResult'
+    })
+  }
+};
+</script>
