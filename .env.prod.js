0fcfded321650020e54e7b1c05cf6b3ad224ef96
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-30 00:50:20 +0200

    tmp

--- .env.prod.js
+++ .env.prod.js
@@ -4,6 +4,6 @@ export default {
   appUrl: 'https://memorypro.herokuapp.com',
   apiUrl: 'https://memorypro-api.herokuapp.com',
 
-  client_id: 3,
-  client_secret: '8IhtL8FJdKWtrkZXETJ4cKJJHw5AeYW6FIy3oq1N',
+  client_id: 15,
+  client_secret: 'hrjcZUlONqqRuiYyH5LlnKKV91P7YRDYrYoXslQh',
 };
