2be2f48751728bf11de875c1b707f7ab1282fa0e
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-30 00:10:12 +0200

    tmp-for-deploy

---
+++ .env.dev.js
@@ -0,0 +1,9 @@
+// You have to create .env.dev.js and .env.prod.js
+
+export default {
+  appUrl: 'http://localhost:3000/',
+  apiUrl: 'http://localhost:8000/',
+
+  client_id: 1,
+  client_secret: 'mhDD10fjZDvITImu...',
+};
---
+++ .env.prod.js
@@ -0,0 +1,9 @@
+// You have to create .env.dev.js and .env.prod.js
+
+export default {
+  appUrl: 'https://memorypro.herokuapp.com/',
+  apiUrl: 'https://memorypro-api.herokuapp.com/',
+
+  client_id: 3,
+  client_secret: '8IhtL8FJdKWtrkZXETJ4cKJJHw5AeYW6FIy3oq1N',
+};
