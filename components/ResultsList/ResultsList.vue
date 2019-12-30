4281b7fc18fd7f2bd2ebd7c2595072d951cd97a1
Dmitry Gashko <dmitry.gashko@postindustria.com>
2019-12-30 03:15:09 +0200

    remove imageResList from ResList

--- components/ResultsList/ResultsList.vue
+++ components/ResultsList/ResultsList.vue
@@ -2,17 +2,15 @@
   <div class="mt-3">
     <NumbersResultsList :userId="userId" :limit="limit" class="item" />
     <WordsResultsList :userId="userId" :limit="limit" class="item" />
-    <ImagesResultsList :userId="userId" :limit="limit" class="item" />
   </div>
 </template>
 
 <script>
 import NumbersResultsList from '@@/ResultsList/NumbersResultsList';
 import WordsResultsList from '@@/ResultsList/WordsResultsList';
-import ImagesResultsList from '@@/ResultsList/ImagesResultsList';
 
 export default {
-  components: { NumbersResultsList, WordsResultsList, ImagesResultsList },
+  components: { NumbersResultsList, WordsResultsList },
   props: ['userId', 'limit']
 };
 </script>
